5ab9a82328c66b71de5c30d95904e2af909dd4e1
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-06-22 13:00:44 -0400

    change graph color

--- graphs.py
+++ graphs.py
@@ -3,9 +3,10 @@ import leather
 def total_case_chart(data):
     leather.theme.axis_title_gap=30
     leather.theme.default_chart_width=1000
+    leather.theme.default_series_colors = ['#87dcce']
 
     chart = leather.Chart('Total COVID-19 Cases in Ontario')
-    chart.add_line(data, stroke_color='#87dcce')
+    chart.add_line(data)
     chart.add_y_axis(name='Number of Cases')
     chart.add_x_axis(name='Days Since Jan 26, 2020', ticks=[i for i in range(0, 150, 10)])
     chart.to_svg('total_cases.svg')
