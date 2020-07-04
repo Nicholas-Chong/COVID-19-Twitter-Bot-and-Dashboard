6314e472cbd094694a4aec0b0fea0b61f3d9b14b
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-07-04 10:49:28 -0400

    Fixed bug ; Procfile now refers to correct vars

--- dashboard.py
+++ dashboard.py
@@ -7,6 +7,7 @@ from models import *
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css'] # Dash CSS
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 app.title = 'Ontario Coronavirus Summary'
 
 data = Daily_Report.select().dicts()
