ad9f9cdaf6c158b17b68d3ac05c45164431998c1
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-07-05 17:27:02 -0400

    Added comments

--- bot.py
+++ bot.py
@@ -16,23 +16,26 @@ import tweets
 def main():
     link = 'https://data.ontario.ca/api/3/action/datastore_search?resource_id=ed270bb8-340b-41f9-a7c6-e8ef587e6d11&limit=1000'
 
+    # Query data through API
     query = urllib.request.urlopen(link)
     query = json.loads(query.read())
 
     results = query['result']['records']
     num_results = len(results) - 1
 
+    # Isolate required data and store in variables
     date = results[num_results]['Reported Date'][0:10]
     new_cases = results[num_results]['Total Cases'] - results[num_results - 1]['Total Cases']
     tests_completed = results[num_results]['Total tests completed in the last day']
     total_cases = results[num_results]['Total Cases'] 
 
+    # Send out the daily update tweet
     tweets.daily_update(
         date, 
         new_cases, 
         total_cases,
         tests_completed,
     )
-    
+
 if __name__ == '__main__':
     main()
