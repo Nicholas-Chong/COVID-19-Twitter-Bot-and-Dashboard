ceec0bd5bca9578d959f5db9799a80c3f43c246c
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-07-21 15:47:00 -0400

    Seperated data generation from html generation (refactoring)

--- dashboard.py
+++ dashboard.py
@@ -11,8 +11,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
-import pandas as pd
-from models import *
+from site_data.get_data import *
 
 # Create Dash app instance
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css'] # Dash CSS
@@ -20,38 +19,12 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title = 'Ontario Coronavirus Summary'
 
-# Query data and return as dictionary
-data = Daily_Report.select().dicts()
-
-# Create dataframe
-df = pd.DataFrame({
-    'Date' : [i['date'] for i in data],
-    'New Cases' : [i['net_new_cases'] for i in data],
-    'Total Cases' : [i['total_cases'] for i in data],
-    'New Deaths' : [i['net_new_deaths'] for i in data],
-    'Total Deaths' : [i['total_deaths'] for i in data],
-    'Tests Completed' : [i['net_new_tests'] for i in data],
-})
-df['7 Day Average'] = df['New Cases'].rolling(window=7).mean()
-df['Percent Positive'] = round((df['New Cases'] / df['Tests Completed']) * 100, 2)
-print(df)
-
 # Create Figures
 fig1 = px.line(data_frame=df, x='Date', y=['New Cases', '7 Day Average'], title='Daily New Cases')
 fig2 = px.line(data_frame=df, x='Date', y='Total Cases', title='Total Cases')
 fig3 = px.line(data_frame=df, x='Date', y='New Deaths', title='Daily New Deaths')
 fig4 = px.line(data_frame=df, x='Date', y='Tests Completed', title='Daily Tests Completed')
 fig5 = px.line(data_frame=df, x='Date', y='Percent Positive', title='Daily Percent Positive')
-
-def day_over_day(today, yesterday):
-    difference = today - yesterday
-    if difference > 0:
-        return [f'+{str(difference)} increase vs yesterday', 'orangered']
-    else:
-        return [f'{str(difference)} decrease vs yesterday', 'limegreen']
-
-dod_new_cases = day_over_day(df.iloc[-1]['New Cases'], df.iloc[-2]['New Cases'])
-dod_new_deaths = day_over_day(df.iloc[-1]['New Deaths'], df.iloc[-2]['New Deaths'])
 
 # Create layout (html generation using dash_html_components)
 app.layout = html.Div(
---
+++ site_data/get_data.py
@@ -0,0 +1,44 @@
+'''----------------------------------------------------------------------------
+Name:        Get Data (get_data.py)
+Purpose:     To query data from the database and transform into a usable pandas 
+             dataframe. Dataframe will be used to present data in Plotly Dash.
+             To be imported by dashboard.py .
+
+Author:      Nicholas Chong
+Created:     2020-07-21 (YYYY/MM/DD)
+----------------------------------------------------------------------------'''
+
+import plotly.express as px
+import pandas as pd
+from models import *
+
+# Function to get day over day variances; pass on the corresponding color
+def day_over_day(today, yesterday):
+    difference = today - yesterday
+    if difference > 0:
+        return [f'+{str(difference)} increase vs yesterday', 'orangered']
+    else:
+        return [f'{str(difference)} decrease vs yesterday', 'limegreen']
+
+# Query data and return as dictionary
+data = Daily_Report.select().dicts()
+
+# Create pandas dataframe
+df = pd.DataFrame({
+    'Date' : [i['date'] for i in data],
+    'New Cases' : [i['net_new_cases'] for i in data],
+    'Total Cases' : [i['total_cases'] for i in data],
+    'New Deaths' : [i['net_new_deaths'] for i in data],
+    'Total Deaths' : [i['total_deaths'] for i in data],
+    'Tests Completed' : [i['net_new_tests'] for i in data],
+})
+
+# Determine 7 day rolling average
+df['7 Day Average'] = df['New Cases'].rolling(window=7).mean()
+
+# Determine daily percent positive
+df['Percent Positive'] = round((df['New Cases'] / df['Tests Completed']) * 100, 2)
+
+# Store day over day increases in lists
+dod_new_cases = day_over_day(df.iloc[-1]['New Cases'], df.iloc[-2]['New Cases'])
+dod_new_deaths = day_over_day(df.iloc[-1]['New Deaths'], df.iloc[-2]['New Deaths'])
