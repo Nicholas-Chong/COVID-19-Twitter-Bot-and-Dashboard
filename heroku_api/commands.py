5ab534fec84bf8aca16bf4bb29f4a843d4f4461d
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-07-24 10:39:35 -0400

    Create heroku api restart command module

---
+++ heroku_api/commands.py
@@ -0,0 +1,10 @@
+import heroku3
+import os
+
+def restart_app():
+    heroku_conn = heroku3.from_key(os.getenv('TOKEN'))
+    app = heroku_conn.apps()['mighty-gorge-03520']
+    return app.restart()
+
+if __name__=='__main__':
+    restart_app()
