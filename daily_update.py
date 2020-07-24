8592692007a4a7271aec6ef0703692e08b8ade97
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-07-24 11:36:16 -0400

    created update function

--- daily_update.py
+++ daily_update.py
@@ -1,3 +1,10 @@
-import site_data.update_data
-import heroku_api.commands
+import site_data.update_data as ud
+import heroku_api.commands as hc
 
+def update():
+    ud.update()
+    hc.restart_app()
+
+
+if __name__=='__main__':
+    update()
