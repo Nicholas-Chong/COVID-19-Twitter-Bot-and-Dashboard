3052b723a1326f5f80a8556748b5230549963853
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-07-24 12:14:24 -0400

    Added docstring header

--- daily_update.py
+++ daily_update.py
@@ -1,6 +1,14 @@
+'''----------------------------------------------------------------------------
+Name:        Daily Update Command (daily_update.py)
+Purpose:     To run the required maintenence commands daily.
+
+Author:      Nicholas Chong
+Created:     2020-06-24 (YYYY/MM/DD)
+----------------------------------------------------------------------------'''
+
 import site_data.update_data as ud
 import heroku_api.commands as hc
-# comment
+
 def main():
     ud.update()
     hc.restart_app()
