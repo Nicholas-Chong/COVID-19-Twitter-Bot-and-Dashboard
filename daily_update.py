76ad93259a4334da9560e57694c3eb5f6d9cb6c2
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-07-24 11:36:30 -0400

    changed function name to main()

--- daily_update.py
+++ daily_update.py
@@ -1,10 +1,10 @@
 import site_data.update_data as ud
 import heroku_api.commands as hc
 
-def update():
+def main():
     ud.update()
     hc.restart_app()
 
 
 if __name__=='__main__':
-    update()
+    main()
