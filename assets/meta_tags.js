917ff38870e19321c4913f93597c1638c23b12f8
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-07-26 11:17:42 -0400

    js to create og:image tag

---
+++ assets/meta_tags.js
@@ -0,0 +1,4 @@
+var meta = document.createElement('meta');
+meta.setAttribute('property', 'og:image');
+meta.content = 'imgs/dashboard_img.png';
+document.getElementsByTagName('head')[0].appendChild(meta);
