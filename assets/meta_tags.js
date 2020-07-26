dfeb94c2602f9df0da438e1e7563bc889e6662eb
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-07-26 11:37:31 -0400

    delete uneeded js

--- assets/meta_tags.js
+++
@@ -1,5 +0,0 @@
-var meta = document.createElement('meta');
-meta.setAttribute('property', 'og:image');
-meta.setAttribute('prefix', 'og: http://ogp.me/ns#');
-meta.content = 'imgs/dashboard_img.png';
-document.getElementsByTagName('head')[0].appendChild(meta);
