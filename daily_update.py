28e4bcb5679f8fa4c33bc7b6bcd85cafbe5fbedc
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-07-28 11:26:42 -0400

    Added twitter daily tweet comand to daily update module

--- daily_update.py
+++ daily_update.py
@@ -6,10 +6,12 @@ Author:      Nicholas Chong
 Created:     2020-06-24 (YYYY/MM/DD)
 ----------------------------------------------------------------------------'''
 
+import twitter.bot as tb
 import site_data.update_data as ud
 import heroku_api.commands as hc
 
 def main():
+    tb.main()
     ud.update()
     hc.restart_app()
 
