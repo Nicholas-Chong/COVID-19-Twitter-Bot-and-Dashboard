78dcd0c62e1d414fd5752a774e81ab3b7180a0e6
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-08-01 13:57:22 -0400

    cleanup

--- assets/clientside_callbacks.js
+++ assets/clientside_callbacks.js
@@ -7,12 +7,10 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
             */
 
             let triggered = dash_clientside.callback_context.triggered[0]
-            
             if (triggered != null) {
                 if ('prop_id' in triggered) {
                     if (triggered['prop_id'] === 'reset_graphs_button.n_clicks') {
                         let message = 'You are viewing dates 2020-01-26 to 2020-08-01';
-    
                         let range = {
                             'start': '2020-01-26',
                             'end': '2020-08-01',
@@ -24,7 +22,6 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
             }
 
             let message = 'You are viewing dates ' + start + ' to ' + end;
-
             let range = {
                 'start': start,
                 'end': end,
