ad900ff6851642cf6139cb813265426ce83b4335
Nicholas-Chong <nicholaschong@Nicholass-MacBook-Pro.local>
2020-08-11 11:54:30 -0400

    clean up and add comments

--- site_data/get_data.py
+++ site_data/get_data.py
@@ -41,22 +41,42 @@ df = pd.DataFrame({
 df['7 Day Average'] = df['New Cases'].rolling(window=7).mean()
 
 # Determine daily percent positive
-df['Percent Positive'] = round((df['New Cases'] / df['Tests Completed']) * 100, 2)
+df['Percent Positive'] = round((df['New Cases']/df['Tests Completed'])*100, 2)
 
 # Store day over day increases in lists
-dod_new_cases = day_over_day(df.iloc[-1]['New Cases'], df.iloc[-2]['New Cases'])
-dod_new_deaths = day_over_day(df.iloc[-1]['New Deaths'], df.iloc[-2]['New Deaths'])
+dod_new_cases = day_over_day(
+    df.iloc[-1]['New Cases'], 
+    df.iloc[-2]['New Cases']
+)
+dod_new_deaths = day_over_day(
+    df.iloc[-1]['New Deaths'], 
+    df.iloc[-2]['New Deaths']
+)
 
 ### DAILY REGIONAL DATA ###
 
 # Query regional data and return as dict
-regional_data = Daily_Regional_Report.select().where(Daily_Regional_Report.date == df['Date'].max()-timedelta(days=1)).dicts()
+regional_data = (
+    Daily_Regional_Report
+    .select()
+    .where(Daily_Regional_Report.date == df['Date'].max()-timedelta(days=1))
+    .dicts()
+)
 df_regional = pd.DataFrame(regional_data).sort_values(by=['total_cases'])
 
 # Query regional data from 2 days ago (for calculating daily increase)
-regional_data2 = Daily_Regional_Report.select().where(Daily_Regional_Report.date == df['Date'].max()-timedelta(days=2)).dicts()
+regional_data2 = (
+    Daily_Regional_Report
+    .select()
+    .where(Daily_Regional_Report.date == df['Date'].max()-timedelta(days=2))
+    .dicts()
+)
 df_regional2 = pd.DataFrame(regional_data2).sort_values(by=['total_cases'])
 
+# Subtract total cases to get day over day increase by region
 regional_increase =  df_regional.copy()
-regional_increase['total_cases'] = df_regional['total_cases'] - df_regional2['total_cases']
+regional_increase['total_cases'] = (
+    df_regional['total_cases']
+    - df_regional2['total_cases']
+)
 regional_increase = regional_increase.sort_values(by=['total_cases'])
