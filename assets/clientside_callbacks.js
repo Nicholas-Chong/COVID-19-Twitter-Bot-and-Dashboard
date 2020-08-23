55d5d133fb9fc407ff82325432922721b1c75452
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2020-08-23 11:19:13 -0400

    Deleted uneeded if statements for reset button Uneeded because the reset button now has own callback (reset_daterange)

--- assets/clientside_callbacks.js
+++ assets/clientside_callbacks.js
@@ -6,22 +6,6 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
             side.
             */
 
-            let triggered = dash_clientside.callback_context.triggered[0]
-            if (triggered != null) {
-                if ('prop_id' in triggered) {
-                    if (triggered['prop_id'] === 'reset_graphs_button.n_clicks') {
-                        let today = new Date().toLocaleDateString('en-CA')
-                        let message = 'You are viewing dates 2020-01-26 to ' + today;
-                        let range = {
-                            'start': '2020-01-26',
-                            'end': today,
-                        }
-
-                        return [message, range]
-                    }
-                }
-            }
-
             let message = 'You are viewing dates ' + start + ' to ' + end;
             let range = {
                 'start': start,
